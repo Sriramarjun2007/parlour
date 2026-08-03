860b175c52a309ae60a1a113f8e8eb51c932cd5b
Sriram Arjun <sriramarjun2007@gmail.com>
2026-08-03 20:32:27 +0530

    updates

--- parlour/user/urls.py
+++ parlour/user/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("services/", views.services, name="services"),
     path("contact/", views.contact, name="contact"),
+    path("booknow/", views.booknow, name="booknow"),
+    path("bookdate/", views.bookdate, name="bookdate"),
 ]
--- parlour/user/views.py
+++ parlour/user/views.py
@@ -11,3 +11,35 @@ def services(request):
 
 def contact(request):
     return render(request, "contact.html")
+
+def booknow(request):
+
+    if request.method == "POST":
+
+        selected_service = request.POST.get("service")
+
+        return render(
+            request,
+            "booknow.html",
+            {
+                "selected_service": selected_service
+            }
+        )
+
+    return render(request, "booknow.html")
+
+def bookdate(request):
+    if request.method == "POST":
+        selected_date = request.POST.get("date")
+        selected_time = request.POST.get("time")
+
+        return render(
+            request,
+            "bookdate.html",
+            {
+                "selected_date": selected_date,
+                "selected_time": selected_time
+            }
+        )
+
+    return render(request, "bookdate.html")
